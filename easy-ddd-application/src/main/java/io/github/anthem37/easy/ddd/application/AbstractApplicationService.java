7bb67c7b698deb6ba9793831ffea457ae01bc44e
maozhihao301 <maozhihao301@hellobike.com>
2025-10-11 20:30:47 +0800

    feat(ddd): 新增应用服务抽象基类
    - 添加了 AbstractApplicationService 类作为应用服务的抽象基类
    - 集成了 ICommandBus、IQueryBus 和 BizFlow 的依赖
    - 提供了统一的命令和查询总线访问方式
    - 简化了应用服务的实现和维护

---
+++ easy-ddd-application/src/main/java/io/github/anthem37/easy/ddd/application/AbstractApplicationService.java
@@ -0,0 +1,23 @@
+package io.github.anthem37.easy.ddd.application;
+
+import io.github.anthem37.easy.ddd.common.cqrs.command.ICommandBus;
+import io.github.anthem37.easy.ddd.common.cqrs.query.IQueryBus;
+import io.github.anthem37.easy.ddd.common.flow.BizFlow;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 应用服务抽象基类
+ *
+ * @author hb28301
+ * @date 2025/10/11 20:28:45
+ */
+@Getter
+@RequiredArgsConstructor
+public abstract class AbstractApplicationService implements IApplicationService {
+
+    private final ICommandBus commandBus;
+    private final IQueryBus queryBus;
+    private final BizFlow bizFlow;
+
+}
